a35cff1f34c1f02e43caeb5fab8811e964352aed
MarieSlz <marie.c.schiltz@gmail.com>
2017-04-08 14:33:25 -0400

    Update Loader

--- lib/helper_load.R
+++ lib/helper_load.R
@@ -28,30 +28,31 @@ clean.record <- function(line, author){
   # Clean Coauhtor List
   for(j in 1:length(coauthor_list)){
     if(nchar(coauthor_list[j])>0){
-      nam = strsplit(coauthor_list[j], " ")[[1]]
+      nam <- strsplit(coauthor_list[j], " ")[[1]]
       if(nchar(nam[1])>0){
-        first.ini=substring(nam[1], 1, 1)
+        first.ini <- substring(nam[1], 1, 1)
       }else{
-        first.ini=substring(nam[2], 1, 1)
+        first.ini <- substring(nam[2], 1, 1)
       }
     }
-    last.name=nam[length(nam)]
-    nam.str = paste(first.ini, last.name)
-    coauthor_list[j]=nam.str
+    last.name <- nam[length(nam)]
+    nam.str <- paste(first.ini, last.name)
+    coauthor_list[j] <- nam.str
   }
   
   # Remove Author from Coauthor list
-  match_ind = charmatch(author, coauthor_list, nomatch=-1)
+  match_ind <- charmatch(author, coauthor_list, nomatch=-1)
   if(match_ind>0){
-    coauthor_list=coauthor_list[-match_ind]
+    coauthor_list <- coauthor_list[-match_ind]
   }
   
+  coauthors <- paste(coauthor_list, collapse=";")
   paper_title <- line.str[2]
   journal_name <- line.str[3]
   
-  return(list(author_id, 
-              paper_id, 
-              coauthor_list, 
-              paper_title, 
-              journal_name))
+  return(list(author.id = author_id, 
+              paper.id = paper_id, 
+              coauthors = coauthors, 
+              paper = paper_title, 
+              journal = journal_name))
 }
