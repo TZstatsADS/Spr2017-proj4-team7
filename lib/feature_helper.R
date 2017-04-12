278d7c942c4d3930113a260bfc1c1338d03c29de
llyyff3742 <yl3594@columbia.edu>
2017-04-12 11:58:43 -0400

    reduce feature dimension

--- lib/feature_helper.R
+++ lib/feature_helper.R
@@ -1,13 +1,27 @@
 ## Coauthor Features
 
 f.coauthor <- function(name) {
+  
+  ## Given a name, produce all coauthor and unique coauthor
   coauthor <- c()
   for (i in 1:length(data_list[[which(query.list==name)]])) {
     coauthor[i] <- data_list[[which(query.list==name)]][[i]][3]
   }
-  
+  coauthor <- unlist(coauthor)
   unique_coauthor <- unique(unlist(coauthor))
   
+  ## Tease out the coauthor that only appears once
+  index <- c()
+  for (i in 1:length(unique_coauthor)) {
+    if (sum(unique_coauthor[i]==coauthor)==1) {
+      index<-index
+    } else {
+      index <- c(index,i)
+    }
+  }
+  unique_coauthor<- unique_coauthor[index]
+  
+  ## Generate features given unique coauthor
   feature_coauthor <- matrix(nrow = length(data_list[[which(query.list==name)]]),ncol = length(unique_coauthor))
   for (i in 1:length(data_list[[which(query.list==name)]])) {
     for (j in 1:length(unique_coauthor)) {
@@ -16,6 +30,7 @@ f.coauthor <- function(name) {
   }
   
   feature_coauthor <- data.frame(feature_coauthor)
+  colnames(feature_coauthor) <- unique_coauthor
   return(feature_coauthor)
 }
 
@@ -24,13 +39,27 @@ f.coauthor <- function(name) {
 ## Journal Features
 
 f.journal <- function(name) {
+  
+  ## Given a name, produce all journals and unique journals
   journal <- c()
   for (i in 1:length(data_list[[which(query.list==name)]])) {
     journal[i] <- data_list[[which(query.list==name)]][[i]][5]
   }
-  
+  journal <- na.omit(unlist(journal))
   unique_journal <- unique(unlist(journal))
   
+  ## Tease out the coauthor that only appears once
+  index <- c()
+  for (i in 1:length(unique_journal)) {
+    if (sum(unique_journal[i]==journal)==1) {
+      index<-index
+    } else {
+      index <- c(index,i)
+    }
+  }
+  unique_journal<- unique_journal[index]
+  
+  ## Generate journal given unique journals
   feature_journal <- matrix(nrow = length(data_list[[which(query.list==name)]]),ncol = length(unique_journal))
   for (i in 1:length(data_list[[which(query.list==name)]])) {
     for (j in 1:length(unique_journal)) {
@@ -39,6 +68,7 @@ f.journal <- function(name) {
   }
   
   feature_journal <- data.frame(feature_journal)
+  colnames(feature_journal) <- unique_journal
   return(feature_journal)
 }
 
@@ -75,3 +105,6 @@ f.ptitle <- function(name) {
 }
 
 
+
+
+  
